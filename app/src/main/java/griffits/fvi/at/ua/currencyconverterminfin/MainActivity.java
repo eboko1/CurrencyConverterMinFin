f707600eea052b4e49e4831af12e13f4c5e61564
Victoria Frantsuh <eboko1@gmail.com>
2017-07-05 22:40:43 +0300

    add update onPostExecute and doInBackground

--- app/src/main/java/griffits/fvi/at/ua/currencyconverterminfin/MainActivity.java
+++ app/src/main/java/griffits/fvi/at/ua/currencyconverterminfin/MainActivity.java
@@ -18,22 +18,26 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.text.DecimalFormat;
 
 public class MainActivity extends Activity {
     private static final String LOG_DEBUG = "MainActivity" ;
-    private TextView usd, eur, rub;
+    private TextView usd, eur, uan;
     private Button converter;
     private EditText enter_et;
     private Spinner spinner_currency;
     private int index_arrayCurrencyData;
     private double inputValue;
 
-    String[] currencyData = {"USD", "EUR", "RUB"};
+    String[] currencyData = {"USD", "EUR", "UAN"};
+    String[] results = new String[10];
 
 
     @Override
@@ -49,7 +53,7 @@ public class MainActivity extends Activity {
     private void init(){
         usd = (TextView)findViewById(R.id.usd_tv);
         eur = (TextView)findViewById(R.id.eur_tv);
-        rub = (TextView)findViewById(R.id.frn_tv);
+        uan = (TextView)findViewById(R.id.frn_tv);
         converter = (Button)findViewById(R.id.button_converter);
         enter_et = (EditText)findViewById(R.id.enter_number);
         spinner_currency = (Spinner)findViewById(R.id.spinner_currency);
@@ -83,13 +87,15 @@ public class MainActivity extends Activity {
     public void btnConverter(View v){
         usd.setText("Waite...");
         eur.setText("Waite...");
-        rub.setText("Waite...");
+        uan.setText("Waite...");
         Log.d(LOG_DEBUG, "onClick btn converter");
 
         String enterValue = enter_et.getText().toString().trim();
-        if (enterValue.length() > 0 && enterValue.equals(".")){
+        if (enterValue.length() > 0 ){  //&& enterValue.equals(".")
             inputValue = Double.parseDouble(enterValue);
-           // new calculate().execuate();
+            Log.d(LOG_DEBUG, "btn converter input value = " + inputValue);
+           new calculate().execute();
+            Log.d(LOG_DEBUG, "start calculate ");
         }
     }
 
@@ -97,19 +103,62 @@ public class MainActivity extends Activity {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            Log.d(LOG_DEBUG, "start method  onPreExecute");
         }
 
         @Override
         protected void onPostExecute(String[] strings) {
-            super.onPostExecute(strings);
+           if (index_arrayCurrencyData == 0){
+               double usdToEurValue, usdToRubValue, usdToUsdValue, usdToEurUan, usdToUan, usdToUanValue;
+
+               // USD to USD
+               usdToUsdValue = inputValue * 1;
+               usd.setText(""+usdToUsdValue);
+                // USD to UAN
+               usdToUanValue = Double.parseDouble(results[0]);
+               usdToUan = inputValue * usdToUanValue;
+               uan.setText(""+usdToUan);
+               // USD to EUR
+               DecimalFormat decimalFormat = new DecimalFormat("0.00");
+               usdToEurValue = Double.parseDouble(results[1]);
+               usdToEurUan = (usdToUanValue/usdToEurValue)*inputValue;
+               eur.setText(""+decimalFormat.format(usdToEurUan));
+
+           } else if (index_arrayCurrencyData == 1){
+
+           } else if (index_arrayCurrencyData == 2){
+
+           }
+            Log.d(LOG_DEBUG, "start method  onPostExecute");
         }
 
         @Override
         protected String[] doInBackground(String... params) {
+            Log.d(LOG_DEBUG, "start method  doInBackground");
             if (index_arrayCurrencyData == 0){
-                String url_;
+                String uRl;
+                try {
+                    uRl = getJson("http://api.minfin.com.ua/summary/b03af6a10c910fb83692634f77f046021a210bcf");
+
+                    JSONObject jsonObject = new JSONObject(uRl);
+
+                    JSONObject usdJSON = jsonObject.getJSONObject("usd");
+                    JSONObject eurJSON = jsonObject.getJSONObject("eur");
+                    results[0] = usdJSON.getString("ask");
+                    results[1] = eurJSON.getString("ask");
+
+                    Log.d(LOG_DEBUG, "results " + " usd: "+results[0]+"\n"  +"EUR "+results[1]);
+                } catch (JSONException e){
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (index_arrayCurrencyData == 1){
+
+            } else if(index_arrayCurrencyData == 2){
+
             }
-            return new String[0];
+            return results;
         }
     }
 
